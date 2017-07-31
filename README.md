86fbe02fddd8657cc5156915ec94cc7c766dfd53
Muthu <muthukumaraswamy.b@2adpro.com>
2017-07-31 10:26:43 +0530

    Code commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # universal-rendering-react
+Example of how to create a bundle of your server side code with Webpack 2, and use it in a Universal React application.
+
+
+## Build
+
+```
+$ npm install
+$ npm run build
+```
+
+## Run
+
+```
+$ cd dist
+$ node server.bundle.js
+```
+
+Go to http://localhost:3000 in your browser
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+const clientConfig = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'public/client.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+};
+
+
+const serverConfig = {
+    entry: './server.js',
+    target: "node",
+    devtool: "source-map",
+    externals: [nodeExternals()],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'server.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: 'require("source-map-support").install();',
+            raw: true,
+            entryOnly: false
+        })
+    ]
+};
+
+module.exports = [clientConfig, serverConfig];
